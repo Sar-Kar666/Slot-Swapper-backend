43555dadfb7fc07abea8b424d2d0225f2a33c0bb
Diptanu Sarkar <diptanusarkar699@gmail.com>
2025-11-06 14:17:59 +0530

    updated

--- server.js
+++ server.js
@@ -16,7 +16,16 @@ initializeSocket(httpServer);
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // local dev
+      "https://slot-swapper-frontend-eight.vercel.app" // your deployed frontend
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 mongoose
